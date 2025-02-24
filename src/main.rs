5ec91c422c22a6c042dcdf6c664aba3e9b6655fd
lazythe <rabidskullgames@gmail.com>
2025-02-24 11:57:23 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
+fn main() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+
+    let stdin = io::stdin();
+    let mut input = String::new();
+    stdin.read_line(&mut input).unwrap();
+    println!("{}: command not found", input.trim());
+}
